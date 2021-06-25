68c8c096945b618258f1cbc14b1d7f53e0980b03
D1ARK-VA4U3 <86417041+D1ARK-VA4U3@users.noreply.github.com>
2021-06-25 07:21:30 +0600

    Update Darkvau.sh

--- Darkvau.sh
+++ Darkvau.sh
@@ -5,7 +5,7 @@ echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m TBomb \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.speedx ]];then
+if [[ -s update.D1ark-va4u3 ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By D@ark v@u >update.D1ark-va4u3
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -24,13 +24,13 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet DarkvauSmS bomber
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border D1ARK-VA4U3
+echo -e "\e[4;34m This Bomber Was Created By D1ARK-VA4U3 \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: nijhum.st.cm.bk.2018@gmail.com \e[0m"
+echo -e "\e[4;32m   Facebook Account: https://www.facebook.com/dark.vau.its.official \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -50,7 +50,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By D1ARK-VA4U3'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -58,28 +58,28 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
+git clone https://github.com/D1ARK-VA4U3/dark-smsbomber-vau
+if [[ -s dark-smsbomber-vau/Darkvau.sh ]];then
 cd TBomb
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm -rf  Darkvau >> temp
+rm update.D1ARK-VA4U3 >> temp
 rm temp
-chmod +x TBomb.sh
+chmod +x Darkvau.sh
 fi
 echo -e "\e[1;32m TBomb Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
-./TBomb.sh
+./Darkvau.sh
 exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
-figlet TBomb
+figlet dark-smsbomber-vau
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border D1ARK-VA4U3
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -89,14 +89,14 @@ echo "  [+] Protection List"
 echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
-echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
+echo -e "\e[1;33m      [*]  D1ARK-VA4U3   \e[1;31m"
+echo "         [-] Mail At: nijhum.st.cm.bk.2018@gmail.com"
+echo -e "\e[1;33m      [*]  FROM BCA TEAM   \e[1;31m"
+echo "         [-] Ping At: https://www.facebook.com/dark.vau.its.official"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
-echo "         [-] Ping At: https://t.me/Rieltar"
+echo "         [-] Ping At: dark.vau.its.official"
 echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
-echo "         [-] Mail At: 0n1cOn3@gmx.ch"
+echo "         [-] Mail At: nijhum.st.cm.bk.2018@gmail.com"
 echo ""
 echo ""
 echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
@@ -106,9 +106,9 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: nijhum.st.cm.bk.2018@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/dark143vau \e[0m"
+echo -e "\e[4;32m   Facebook Account: https://www.facebook.com/dark.vau.its.official \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
@@ -117,11 +117,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border D1ARK-VA4U3
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: nijhum.st.cm.bk.2018@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/dark143vau \e[0m"
+echo -e "\e[4;32m   Facebook account: https://www.facebook.com/dark.vau.its.official \e[0m"
 echo " "
 exit 0
 else
